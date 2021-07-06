62d900e250ad574d20f713cf9065fb5c799750c0
artbat6 <arthurbatesdev@gmail.com>
2021-07-06 16:58:45 -0500

    working on components

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,11 @@
+import "./contact.scss";
+
+function Contact() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Contact
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,16 @@
+import "./topbar.scss";
+
+function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="intro">asas</a>
+                </div>
+                <div className="right"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Topbar

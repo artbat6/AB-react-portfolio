ba5d2d70531740c61aa0162f63ce05886fb019f7
artbat6 <arthurbatesdev@gmail.com>
2021-07-06 18:28:58 -0500

    Update Topbar.jsx

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,32 +1,32 @@
 import "./topbar.scss";
-import PersonIcon from '@material-ui/icons/Person';
-import MailIcon from '@material-ui/icons/Mail';
+import PersonIcon from "@material-ui/icons/Person";
+import MailIcon from "@material-ui/icons/Mail";
 
 export default function Topbar({ menuOpen, setMenuOpen }) {
-    return (
-        <div className={ "topbar " + (menuOpen && "active")>
-            <div className="wrapper">
-                <div className="left">
-                    <a href="#works" className="logo">Arthur Bates</a>
-                    <div className="itemContainer">
-                      <PersonIcon className="icon" />
-                      <span>701 415 8812</span>   
-                    </div>
-                    <div className="itemContainer">
-                      <MailIcon className="icon" />
-                      <span>arthurbatesdev@gmail.com</span>   
-                    </div>
-                </div>
-                <div className="right">
-                    <div className="hamburger">
-                        <span className="line1"></span>
-                        <span className="line2"></span>
-                        <span className="line3"></span>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            genius.
+          </a>
+          <div className="itemContainer">
+            <PersonIcon className="icon" />
+            <span>+44 924 12 74</span>
+          </div>
+          <div className="itemContainer">
+            <MailIcon className="icon" />
+            <span>safak@genius.com</span>
+          </div>
         </div>
-    );
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default Topbar

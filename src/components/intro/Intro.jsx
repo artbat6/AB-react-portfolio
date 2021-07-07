e679bebb1a2914f241cd9bc4088c672299490b86
artbat6 <arthurbatesdev@gmail.com>
2021-07-07 15:53:39 -0500

    intro images

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,21 @@ import "./intro.scss"
 function Intro() {
     return (
         <div className="intro" id="intro">
-
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/arthur.png" alt="photo of Arthur"/>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hello, I'm</h2>
+                    <h1>Arthur Bates</h1>
+                    <h3>Web Developer<span></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/eyes-arrows.png" alt="eyes looking down"/>
+                </a>
+            </div>
         </div>
     )
 }

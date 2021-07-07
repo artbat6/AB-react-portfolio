625a8a4571428ab08ea197363eba1e70f5d60926
artbat6 <arthurbatesdev@gmail.com>
2021-07-07 16:52:22 -0500

    intro text animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,25 +1,36 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
-function Intro() {
+export default function Intro() {
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+          showCursor: true,
+          backDelay: 1500,
+          backSpeed:60,
+          strings: ["Web Developer", "Designer", "Creator"],
+        });
+      }, []);
+
     return (
         <div className="intro" id="intro">
             <div className="left">
                 <div className="imgContainer">
-                    <img src="assets/arthur.png" alt="photo of Arthur"/>
+                    <img src="assets/arthur.png" alt="Arthur"/>
                 </div>
             </div>
             <div className="right">
                 <div className="wrapper">
                     <h2>Hello, I'm</h2>
                     <h1>Arthur Bates</h1>
-                    <h3>Web Developer<span></span></h3>
+                    <h3>Independent <span ref={textRef}></span></h3>
                 </div>
                 <a href="#portfolio">
-                    <img src="assets/eyes-arrows.png" alt="eyes looking down"/>
+                    <img src="assets/down.png" alt="arrow"/>
                 </a>
             </div>
         </div>
-    )
+    );
 }
-
-export default Intro

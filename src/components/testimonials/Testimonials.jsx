14948c77111699e92df1b16f76e2fb72cc779b7a
artbat6 <arthurbatesdev@gmail.com>
2021-07-10 16:42:52 -0500

    testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,11 +1,32 @@
-import "./testimonials.scss"
+import "./testimonials.scss";
 
 function Testimonials() {
-    return (
-        <div className="testimonials" id="testimonials">
-
+  return (
+    <div className="testimonials" id="testimonials">
+      <h1>Testimonials</h1>
+      <div className="container">
+        <div className="card">
+          <div className="top">
+            <img src="assets/right-arrow.png" className="left" alt="" />
+            <img
+              src="https://leviathyn.com/wp-content/uploads/2013/06/41.jpg"
+              alt="tobias"
+            />
+            <img src="assets/youtube.png" alt="" />
+          </div>
+          <div className="center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+            porro, nisi illo magni corrupti delectus minima dolor est eum
+            eveniet!
+          </div>
+          <div className="bottom">
+              <h3>Tobias Funke</h3>
+              <h4>Actor</h4>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Testimonials
+export default Testimonials;

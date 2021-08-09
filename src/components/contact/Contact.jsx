c963999696f367f201d8dca526ac79502d394212
artbat6 <arthurbatesdev@gmail.com>
2021-08-09 17:57:07 -0500

    adjusting contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -16,7 +16,7 @@ export default function Contact() {
       <div className="right">
         <h2>Contact.</h2>
         <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email" />
+          <input class="input-text" type="text" placeholder="Email" />
           <textarea placeholder="Message"></textarea>
           <button type="submit">Send</button>
           {message && <span>Thanks, I'll reply ASAP :)</span>}

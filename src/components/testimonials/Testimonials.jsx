9d3e1ddfd551fc0a2345306db0226945785db074
artbat6 <arthurbatesdev@gmail.com>
2021-08-23 12:53:31 -0500

    added chef Jason testimonial

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -4,29 +4,29 @@ export default function Testimonials() {
   const data = [
     {
       id: 1,
-      name: "Tom Durden",
-      title: "Senior Developer",
-      img: "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      icon: "assets/twitter.png",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.",
+      name: "Jason Kerr",
+      title: "Owner Little Kitchen HTX",
+      img: "assets/jason.png",
+      // icon: "assets/twitter.png",
+      desc: "Arthur Bates is a thinker. Quick on his feet and an absolute pleasure to work with. Reliable and professional to the teeth. Arthur is one of the most creative employees I’ve ever had the honor to work with and is always striving to improve his skills."
     },
-    {
-      id: 2,
-      name: "Alex Kalinski",
-      title: "Co-Founder of DELKA",
-      img: "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      icon: "assets/youtube.png",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
-      featured: true,
-    },
-    {
-      id: 3,
-      name: "Martin Harold",
-      title: "CEO of ALBI",
-      img: "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      icon: "assets/linkedin.png",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
-    },
+    // {
+    //   id: 2,
+    //   name: "Alex Kalinski",
+    //   title: "Co-Founder of DELKA",
+    //   img: "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    //   icon: "assets/youtube.png",
+    //   desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
+    //   featured: true,
+    // },
+    // {
+    //   id: 3,
+    //   name: "Martin Harold",
+    //   title: "CEO of ALBI",
+    //   img: "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    //   icon: "assets/linkedin.png",
+    //   desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
+    // },
   ];
   return (
     <div className="testimonials" id="testimonials">

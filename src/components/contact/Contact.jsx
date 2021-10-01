453627a601b4eced86af6dca102861ff974b990e
artbat6 <arthurbatesdev@gmail.com>
2021-10-01 16:44:58 -0500

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -17,7 +17,7 @@ export default function Contact() {
         <h2>Contact me</h2>
         <a className="linkedin" href="https://www.linkedin.com/in/arthur-bates-824b5476/" target="_blank">Linkedin</a>
         <a className="email" href="mailto: arthurbatesdev@gmail.com" target="_blank">Email</a>
-        <a className="resume" href="https://docs.google.com/document/d/1_NgROtJZ8JsrEC_RO8Z2NdFEhnVunKdvSDHI9EdPjwI/edit?usp=sharing" target="_blank">Resume</a>
+        <a className="resume" href="https://docs.google.com/document/d/1mmrL2j8bF7BLw2mtdZ_HJfBEpLO5LaWGpmfgA4lZnEU/edit?usp=sharing" target="_blank">Resume</a>
 
     
       </div>

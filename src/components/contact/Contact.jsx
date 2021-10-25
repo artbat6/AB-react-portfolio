946bb1768c34269356202701d5ae9c84bba6d360
artbat6 <arthurbatesdev@gmail.com>
2021-10-25 16:43:49 -0500

    styling contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -11,10 +11,11 @@ export default function Contact() {
   return (
     <div className="contact" id="contact">
       <div className="left">
-        <img src="assets/mail-pole.svg" alt="" />
+        <img className="curvy" src="assets/mail-pole.svg" alt="mailbox illustration" />
       </div>
       <div className="right">
         <h2>Contact me</h2>
+        <img src="assets/curvyUnderLine.svg" alt="curvy underline"/>
         <a className="linkedin" href="https://www.linkedin.com/in/arthur-bates-824b5476/" target="_blank">Linkedin</a>
         <a className="email" href="mailto: arthurbatesdev@gmail.com" target="_blank">Email</a>
         <a className="resume" href="https://docs.google.com/document/d/1mmrL2j8bF7BLw2mtdZ_HJfBEpLO5LaWGpmfgA4lZnEU/edit?usp=sharing" target="_blank">Resume</a>
